a9e3d814ec4167c415bed58524283aae3ab51dc2
gfpaiva <gfxpaiva@gmail.com>
2026-01-16 02:09:34 -0300

    chore: add commitlint hook

---
+++ commitlint.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            ['feat', 'fix', 'refactor', 'docs', 'chore'],
+        ],
+        'subject-case': [2, 'always', 'lower-case'],
+        'subject-full-stop': [2, 'never', '.'],
+        'type-case': [2, 'always', 'lower-case'],
+        'type-empty': [2, 'never'],
+        'subject-empty': [2, 'never'],
+    },
+};
